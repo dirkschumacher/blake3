39c3c2beb237c34fe9a3b71220c4ba8003799f87
Dirk Schumacher <mail@dirk-schumacher.net>
2020-01-12 16:22:28 +0100

    Use just one stopifnot

--- R/hash.R
+++ R/hash.R
@@ -3,7 +3,6 @@
 #' @param key either NULL or a 32 byte long raw vector
 #' @export
 blake3_hash_raw <- function(x, key = NULL) {
-  stopifnot(is.raw(x))
-  stopifnot(is.null(key) || (is.raw(key) && length(key) == 32L))
+  stopifnot(is.raw(x), is.null(key) || (is.raw(key) && length(key) == 32L))
   .Call("hash", x, key)
 }
